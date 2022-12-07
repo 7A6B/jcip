0755a52cf02eedd9e3316ea4731e1d05cc858951
7A6B <zk_focus@sina.com>
2022-12-07 19:53:15 +0800

    多个线程访问同一个持久化对象

--- javase-concurrency-in-practice/src/main/java/com/jcip/chapter2/EvenChecker.java
+++ javase-concurrency-in-practice/src/main/java/com/jcip/chapter2/EvenChecker.java
@@ -12,7 +12,6 @@ public class EvenChecker implements Runnable{
         id = identity;
         this.generator = generator;
     }
-
     @Override
     public void run() {
         while (!generator.isCanceled()){
